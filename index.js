0d2b94228dbd3745787350a386f312d10f7e5a04
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-07 20:00:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    drivers = drivers.slice(0,2)
+    return drivers
+}
+
+
+function returnLastTwoDrivers(drivers){
+    drivers = drivers.slice(-2)
+    return drivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(num){
+    return totalFare => totalFare * num
+}
+
+function fareDoubler(createFareMultiplier){
+        return createFareMultiplier * 2 
+}
+
+function fareTripler(createFareMultiplier){
+    return createFareMultiplier * 3
+}
+
+function selectDifferentDrivers(drivers, callBack){
+    return callBack(drivers)
+}
